ecc3c5049ee7cc6da092e9992a1a2fae1393eb25
bje <baejungeun11@gmail.com>
2025-09-03 13:27:47 +0900

    config: kafka.yml에 정의한 토픽을 객체화 할 수 있는 ConfigurationProperties 작성

---
+++ modules/kafka/src/main/java/com/loopers/config/kafka/KafkaTopicsProperties.java
@@ -0,0 +1,14 @@
+package com.loopers.config.kafka;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Setter
+@Getter
+@Component
+@ConfigurationProperties(prefix = "commerce-kafka.topics")
+public class KafkaTopicsProperties {
+    private String like;
+}
