f16c480d9ad0b42187415badec82fdef07781aa4
bje <baejungeun11@gmail.com>
2025-09-04 12:47:51 +0900

    feat:   @KafkaHandler 사용으로 변경 : 작동안되는 상황

--- apps/commerce-streamer/src/main/java/com/loopers/interfaces/consumer/LikeCountKafkaConsumer.java
+++ apps/commerce-streamer/src/main/java/com/loopers/interfaces/consumer/LikeCountKafkaConsumer.java
@@ -1,46 +1,34 @@
 package com.loopers.interfaces.consumer;
 
 import com.loopers.config.kafka.KafkaTopicsProperties;
-import com.loopers.domain.metrics.LikeEvent;
 import com.loopers.domain.metrics.ProductMetricsService;
+import com.loopers.domain.sharedkernel.LikeEvent;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaHandler;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.Acknowledgment;
-import org.springframework.messaging.MessageHeaders;
-import org.springframework.messaging.handler.annotation.Headers;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @RequiredArgsConstructor
 @Component
+@KafkaListener(topics = "#{@kafkaTopicsProperties.like}")
 public class LikeCountKafkaConsumer {
     private final KafkaTopicsProperties topics;
     private final ProductMetricsService productMetricsService;
 
-    @KafkaListener(topics = "#{@kafkaTopicsProperties.like}")
+
+    @KafkaHandler
     public void likeCountListener(
-            @Payload LikeEvent.LikeCountIncreased event,
-            @Headers MessageHeaders headers,
+            LikeEvent.LikeCountIncreased event,
             ConsumerRecord<String, Object> messages,
             Acknowledgment acknowledgment
     ) {
         log.info("***message:{}", messages);
         productMetricsService.increaseLikeCount(event);
-        acknowledgment.acknowledge(); // manual ack
-    }
-
-    @KafkaListener(topics = "#{@kafkaTopicsProperties.like}")
-    public void likeCountDecreaseListener(
-            @Payload LikeEvent.LikeCountDecreased event,
-            @Headers MessageHeaders headers,
-            ConsumerRecord<String, Object> messages,
-            Acknowledgment acknowledgment
-    ) {
-        log.info("***message:{}", messages);
-        productMetricsService.decreaseLikeCount(event);
         acknowledgment.acknowledge(); // manual ack
     }
 }
